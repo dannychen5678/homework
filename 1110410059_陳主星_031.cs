5e7cf52d2a6d12b2d1f8e9fe0e8859b88c277b53
dannychen5678 <166821393+dannychen5678@users.noreply.github.com>
2024-04-12 19:45:12 +0800

    Add files via upload

---
+++ 1110410059_陳主星_031.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static List<int> FindWordsWithCharacter(string[] words, char x)
+    {
+        List<int> result = new List<int>();
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].Contains(x))
+            {
+                result.Add(i);
+            }
+        }
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        string[] words = { "leet", "code" };
+        char x = 'e';
+        List<int> indices = FindWordsWithCharacter(words, x);
+        foreach (int index in indices)
+        {
+            Console.WriteLine(index);
+        }
+    }
+}
